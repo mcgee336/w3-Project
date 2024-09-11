adbbd4241e9a480e92d01c11d2c5afe8542bcf93
John McGee <mcgee336@morrisville.edu>
2024-09-11 15:16:11 -0400

    C# Formatting & Input.GetAxis()

--- w3 Project/Assets/Mover.cs
+++ w3 Project/Assets/Mover.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0;
+    
+    // These Variables change the players speed
     
     // Start is called before the first frame update
     void Start()
@@ -17,6 +16,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue); 
+        float xValue = Input.GetAxis ("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0, zValue); 
     }
 }
