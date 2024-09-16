8dc14e0cd54eb9adc0428e18f77e8c91acae9ffb
John McGee <mcgee336@morrisville.edu>
2024-09-16 15:50:27 -0400

    If Statements

--- w3 Project/Assets/Scripts/Dropper.cs
+++ w3 Project/Assets/Scripts/Dropper.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+
+    [SerializeField] float timeToWait = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class Dropper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.time);
+        if(Time.time > timeToWait)
+        {
+            Debug.Log("3 Seconds has passed");
+        }
     }
 }
