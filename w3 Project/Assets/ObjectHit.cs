c8709a7ee95c17c3f6045003b8e66259559ecd37
John McGee <mcgee336@morrisville.edu>
2024-09-16 15:40:05 -0400

    Incrementing a Score

--- w3 Project/Assets/ObjectHit.cs
+++ w3 Project/Assets/ObjectHit.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+  
+    
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log("Bumped into a wall");
         GetComponent<MeshRenderer>().material.color = Color.red;
     }
 }
