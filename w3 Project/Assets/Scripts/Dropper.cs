2469bb25f54af1052c48bce2f4d55ac3d64fdd52
John McGee <mcgee336@morrisville.edu>
2024-09-16 16:03:03 -0400

    Caching a Reference

--- w3 Project/Assets/Scripts/Dropper.cs
+++ w3 Project/Assets/Scripts/Dropper.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
-
+    MeshRenderer renderer;
+    Rigidbody rigidbody;
     [SerializeField] float timeToWait = 5f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidbody.useGravity = false;
     }
 
     // Update is called once per frame
@@ -18,7 +23,8 @@ public class Dropper : MonoBehaviour
     {
         if(Time.time > timeToWait)
         {
-            Debug.Log("3 Seconds has passed");
+            renderer.enabled = true;
+            rigidbody.useGravity = true;
         }
     }
 }
